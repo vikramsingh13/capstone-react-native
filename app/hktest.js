01a5aebd759a70f5cf91fcd0b4456109c2405681
Vincent Ursino <91570705+Vurs@users.noreply.github.com>
2023-04-08 10:04:43 -0400

    Fixed errors with getting project to run. Now compiles and runs fine. In the process of making a dev build.

--- app/hktest.js
+++ app/hktest.js
@@ -1,12 +1,7 @@
-import { HKQuantityTypeIdentifier, useHealthkitAuthorization } from '@kingstinct/react-native-healthkit';
-import useMostRecentQuantitySample from '@kingstinct/react-native-healthkit/lib/typescript/src/hooks/useMostRecentQuantitySample';
+import { useMostRecentQuantitySample, HKQuantityTypeIdentifier, useHealthkitAuthorization, useMostRecentCategorySample } from '@kingstinct/react-native-healthkit';
 
-function hktest(){
+function hktest() {
   //make sure you do this before you try to access any data or else it will crash
   const [authorizationStatus, requestAuthorization] = useHealthkitAuthorization([HKQuantityTypeIdentifier.bloodGlucose])
-
-  const bodyFat = useMostRecentQuantitySample(HKQuantityTypeIdentifier.bodyFatPercentage)
-  const height = useMostRecentQuantitySample(HKQuantityTypeIdentifier.height)
-  //this is how we sample heartrate on ios
-  const HeartRate = useMostRecentQuantitySample(HKQuantityTypeIdentifier.heartRate)
+  const bodyFat = useMostRecentQuantitySample(HKQuantityTypeIdentifier.bloodGlucose)
 }
