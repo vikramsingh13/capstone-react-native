3294c353dbcf69bc264d77425d9b19ed46672199
Eric <eric050117@outlook.com>
2023-05-28 14:58:20 -0400

    created global layout

--- app/_layout.js
+++ app/_layout.js
@@ -1,26 +1,53 @@
-import { Stack } from "expo-router";
-import { useCallback } from "react";
-import { useFonts } from "expo-font";
+import React, { useCallback } from 'react';
+import { SafeAreaView, ScrollView, View } from 'react-native';
+import { Stack } from 'expo-router';
+import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
 
-SplashScreen.preventAutoHideAsync();
+import { COLORS, icons, images, SIZES } from "../constants";
 
-const Layout = () => {
-    const [fontsLoaded] = useFonts({
-        DMBold: require('../assets/fonts/DMSans-Bold.ttf'),
-        DMMedium: require('../assets/fonts/DMSans-Medium.ttf'),
-        DMRegular: require('../assets/fonts/DMSans-Regular.ttf'),
-    })
 
-    const onLayoutRootView = useCallback(async() => {
-        if(fontsLoaded){
-            await SplashScreen.hideAsync();
-        }
-    }, [fontsLoaded]);
+const Layout = ({ children }) => {
+  const [fontsLoaded] = useFonts({
+      DMBold: require('../assets/fonts/DMSans-Bold.ttf'),
+      DMMedium: require('../assets/fonts/DMSans-Medium.ttf'),
+      DMRegular: require('../assets/fonts/DMSans-Regular.ttf'),
+  });
 
-    if(!fontsLoaded) return null;
+  const onLayoutRootView = useCallback(async() => {
+      if (fontsLoaded) {
+          await SplashScreen.hideAsync();
+      }
+  }, [fontsLoaded]);
 
-    return <Stack onLayout={onLayoutRootView} />;
-};
+  if (!fontsLoaded) return null;
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.background }}>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.primary },
+          headerShadowVisible: false,
+          headerRight: () => (
+            <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" />
+          ),
+          headerTitle: '',
+        }}
+      />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View
+          style={{
+            flex: 1,
+            padding: SIZES.medium,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          {children}
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+
+  );
+}
 
 export default Layout;
